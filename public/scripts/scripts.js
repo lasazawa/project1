1972eec9dd6fd1118c53bc340dd387ee4a151841
= <lasazawa@gmail.com>
2014-12-12 16:51:54 -0800

    change date and update db

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -12,47 +12,55 @@ $(document).ready(function(){
   defaultDate();
 
 
-  // $(".date").on("click",function(event){
-  //   $("#datepicker").datepicker.show();
-  // });
- // $(function() {
- //    $( "#datepicker" ).datepicker();
- //  });
-
-  // datepicker
-  $("#datepicker").datepicker( {
-      onSelect: function() {
-        var dateObject = $(this).datepicker('getDate');
-        var selectedDate = $.datepicker.formatDate( "yy-mm-dd", dateObject);
-        var showDate = $.datepicker.formatDate("DD, MM d", dateObject);
-        console.log(dateObject);
-        console.log(selectedDate);
-        console.log(showDate);
-        $(this).val(showDate);
-        $(this).attr('size', $(this).val().length+6);
-        var _this = $(this);
-
-        $.ajax({
-          type: "POST",
-          url: "/home/date",
-          data: {date: selectedDate}
-        }).done(function(response) {
-          console.log("THIS IS THE RESPONSE: " + response);
-          $('#home-main').html(response);
-          console.log(_this);
-          console.log(showDate);
-          $('#datepicker').val(showDate);
-          $('#datepicker').attr('size', $('#datepicker').val().length+5);
-          $('#datepicker').addClass("hasDatepicker");
-
-
-          // STILL NEED TO UPDATE NUMBER OF EVENTS
-          // IF ARTIST NAME IS ALL CAPS ITS NOT FINDING SPOTIFY NAME
-        });
-      }
+  // // datepicker
+  function dateHandler() {
+    var dateObject = $(this).datepicker('getDate');
+    var selectedDate = $.datepicker.formatDate( "yy-mm-dd", dateObject);
+    var showDate = $.datepicker.formatDate("DD, MM d", dateObject);
+    $(this).val(showDate);
+    $(this).attr('size', $(this).val().length+6);
+    var _this = $(this);
+    $(".eventList").load("/home/date .eventList", {date: selectedDate}, function(response, status, xhr){
+      console.log(response);
+    });
+    $('#num-of-events').load("/home/date #num-of-events", {date: selectedDate}, function(response, status, xhr){
+      console.log(response);
     });
 
-  // get list of events based on date selecte
+  }
+
+  $("#datepicker").datepicker( {
+    onSelect: dateHandler
+  });
+  // $("#datepicker").datepicker( {
+  //     onSelect: function() {
+  //       var dateObject = $(this).datepicker('getDate');
+  //       var selectedDate = $.datepicker.formatDate( "yy-mm-dd", dateObject);
+  //       var showDate = $.datepicker.formatDate("DD, MM d", dateObject);
+  //       console.log(dateObject);
+  //       console.log(selectedDate);
+  //       console.log(showDate);
+  //       $(this).val(showDate);
+  //       $(this).attr('size', $(this).val().length+6);
+
+  //       $.ajax({
+  //         type: "POST",
+  //         url: "/home/date",
+  //         data: {date: selectedDate}
+  //       }).done(function(response) {
+  //         console.log("THIS IS THE RESPONSE: " + response);
+  //         $('#home-main').html(response);
+
+  //         $('#datepicker').val(showDate);
+  //         $('#datepicker').attr('size', $('#datepicker').val().length+6);
+  //         $('#datepicker').addClass("hasDatepicker");
+
+  //         // STILL NEED TO UPDATE NUMBER OF EVENTS
+  //         // IF ARTIST NAME IS ALL CAPS ITS NOT FINDING SPOTIFY NAME
+  //       });
+  //     }
+  //   });
+
 
 
   //Check to see if the window is top if not then display button
